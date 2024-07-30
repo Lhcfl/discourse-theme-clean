f8e88271c6da5eac7ab596efec2390bd0e612393
Lhc_fl <linca@discourse.org>
2024-07-30 16:38:48 +0800

    pretty

--- javascripts/discourse/d-nav-controls/initializers/init-nav-controls.js
+++ javascripts/discourse/d-nav-controls/initializers/init-nav-controls.js
@@ -7,7 +7,7 @@ export default {
     withPluginApi("0.8.13", (api) => {
       const site = api.container.lookup("site:main");
       if (!site.mobileView) return;
-            
+
       let scrollTop = window.scrollY;
       const body = document.body;
       const scrollMax = 0;
@@ -15,20 +15,21 @@ export default {
       const hiddenNavClass = "nav-controls-hidden";
 
       const add_class_on_scroll = () => body.classList.add(hiddenNavClass);
-      const remove_class_on_scroll = () => body.classList.remove(hiddenNavClass);
+      const remove_class_on_scroll = () =>
+        body.classList.remove(hiddenNavClass);
 
-      window.addEventListener('scroll', function() { 
+      window.addEventListener("scroll", function () {
         scrollTop = window.scrollY;
         if (
           lastScrollTop < scrollTop &&
           scrollTop > scrollMax &&
           !body.classList.contains(hiddenNavClass)
-        ) { 
+        ) {
           add_class_on_scroll();
         } else if (
           lastScrollTop > scrollTop &&
           body.classList.contains(hiddenNavClass)
-        ) { 
+        ) {
           remove_class_on_scroll();
         }
         lastScrollTop = scrollTop;
